3c9031368bd4500c6aa1d2052b81c0ceb984dd9e
kateNEO <glebova.caterina2012@yandex.ru>
2025-07-13 23:54:43 +0200

    chore: adding gh-pages for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/rs-school-react-2025/',
   plugins: [react(), tailwindcss()],
 });
