60e637fa6cc2b5be8d26bf0a9b4a5d7c6d8d5276
kateNEO <glebova.caterina2012@yandex.ru>
2025-07-23 18:40:28 +0200

    refactor: transformation Search component into a function

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,69 +1,61 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from './Button.tsx';
 import { searchPlanet } from '../services/searchPlanet.ts';
 import { saveToLocalStorage } from '../services/saveToLocalStorage.ts';
 import type { Response } from './MainPage.tsx';
 import { getAllPlanets } from '../services/getAllPlanets.ts';
+
 type SearchProps = {
   onSearch: (response: Response) => void;
   setIsLoading: (value: boolean) => void;
 };
-class Search extends Component<SearchProps> {
-  state = {
-    searchStr: '',
-  };
-  componentDidMount() {
+
+function Search({ onSearch, setIsLoading }: SearchProps) {
+  const [searchStr, setSearchStr] = useState<string>('');
+  useEffect(() => {
     const saved = localStorage.getItem('lastRequest');
     if (saved) {
-      console.log(saved);
-      this.setState({ searchStr: saved });
+      setSearchStr(saved);
     }
-  }
-  handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchStr: e.target.value });
+  }, []);
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchStr(e.target.value);
   };
-  handleClickSearch = async () => {
-    this.props.setIsLoading(true);
+  const handleClickSearch = async () => {
+    setIsLoading(true);
     try {
       let data;
-      if (this.state.searchStr === '') {
+      if (searchStr === '') {
         data = await getAllPlanets();
       } else {
-        data = await searchPlanet(this.state.searchStr.trim());
-        saveToLocalStorage(this.state.searchStr);
+        data = await searchPlanet(searchStr.trim());
+        saveToLocalStorage(searchStr);
       }
-
-      this.props.onSearch(data);
+      onSearch(data);
       console.log(data.result);
 
-      this.props.setIsLoading(false);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
     }
   };
-  render() {
-    return (
-      <div className="w-1/1 h-40 flex justify-between items-center gap-8">
-        <input
-          type="text"
-          value={this.state.searchStr}
-          className="border border-[#9F9F9F] text-white w-full h-10 rounded-[7px] p-4 hover:cursor-pointer hover:shadow-[0_4px_20px_#9ca3af] duration-300"
-          placeholder="Tatooine"
-          onChange={this.handleSearchChange}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              this.handleClickSearch();
-            }
-          }}
-        />
-        <Button
-          text="search"
-          onClick={this.handleClickSearch}
-          disabled={false}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="w-1/1 h-40 flex justify-between items-center gap-8">
+      <input
+        type="text"
+        value={searchStr}
+        className="border border-[#9F9F9F] text-white w-full h-10 rounded-[7px] p-4 hover:cursor-pointer hover:shadow-[0_4px_20px_#9ca3af] duration-300"
+        placeholder="Tatooine"
+        onChange={handleSearchChange}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleClickSearch();
+          }
+        }}
+      />
+      <Button text="search" onClick={handleClickSearch} disabled={false} />
+    </div>
+  );
 }
 
 export default Search;
