8641f6ca05478c6948d8feeb46393f69eeb2e823
kateNEO <glebova.caterina2012@yandex.ru>
2025-08-10 12:09:58 +0200

    feat: adding count of books in csv name

--- src/services/createCSV.ts
+++ src/services/createCSV.ts
@@ -14,7 +14,7 @@ export function createCSV(selectedBooks: BooksCard[]) {
   const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
   const link = document.createElement('a');
   link.href = URL.createObjectURL(blob);
-  link.download = 'books.csv';
+  link.download = `${selectedBooks.length}_selected_books.csv`;
   link.click();
   URL.revokeObjectURL(link.href);
 }
